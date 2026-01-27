ac3fe61a45f1703a2f0d5c5b262cbd1d850bd101
Althirdy <althirdysanger@gmail.com>
2026-01-27 22:00:39 +0800

    Fix/app-log: Adding Dark and Light Logo

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,5 +1,10 @@
 import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return <img src="/favicon.svg" alt="UrbanWatch Logo" />;
+    return (
+        <>
+            <img src="/logoDark.png" alt="UrbanWatch Logo" className="block dark:hidden" />
+            <img src="/logoLight.png" alt="UrbanWatch Logo" className="hidden dark:block" />
+        </>
+    );
 }
--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,7 +1,8 @@
 export default function AppLogo() {
     return (
         <>
-            <img src="/favicon.svg" alt="" className="h-12 w-auto" />
+            <img src="/logoDark.png" alt="UrbanWatch" className="h-8 w-auto block dark:hidden" />
+            <img src="/logoLight.png" alt="UrbanWatch" className="h-8 w-auto hidden dark:block" />
 
             <div className="ml-1 grid flex-1 text-left text-lg">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
